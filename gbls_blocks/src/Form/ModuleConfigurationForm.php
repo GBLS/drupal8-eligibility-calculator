f54ae91cc0d8c2e846a41b49d9c3bfa3e5087d1e
Quinten <quinten@gbls.org>
2019-02-19 21:03:56 -0500

    Allow saving form

--- gbls_blocks/src/Form/ModuleConfigurationForm.php
+++ gbls_blocks/src/Form/ModuleConfigurationForm.php
@@ -32,6 +32,16 @@ class ModuleConfigurationForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('gbls_blocks.settings');
     $form = [
+      'poverty_base' => [
+        '#type' => 'number',
+        '#title' => $this->t('Base poverty rate'),
+        '#default_value' => $config->get('poverty_base')
+      ],
+      'poverty_increment' => [
+        '#type' => 'number',
+        '#title' => $this->t('Poverty rate increment'),
+        '#default_value' => $config->get('poverty_increment')
+      ],
       'checker_title' => [
         '#type' => 'textfield',
         '#title' => $this->t('Page title'),
@@ -42,16 +52,16 @@ class ModuleConfigurationForm extends ConfigFormBase {
         '#title' => $this->t('Page description'),
         '#default_value' => $config->get('checker_description')
       ],
-      'poverty_base' => [
-        '#type' => 'number',
-        '#title' => $this->t('Base poverty rate'),
-        '#default_value' => $config->get('poverty_base')
+      'checker_qualifies' => [
+        '#type' => 'textarea',
+        '#title' => $this->t('Message if the client qualifies'),
+        '#default_value' => $config->get('checker_qualifies')
       ],
-      'poverty_increment' => [
-        '#type' => 'number',
-        '#title' => $this->t('Poverty rate increment'),
-        '#default_value' => $config->get('poverty_increment')
-      ]
+      'checker_disqualifies' => [
+        '#type' => 'textarea',
+        '#title' => $this->t("Message if the client doesn't qualify"),
+        '#default_value' => $config->get('checker_disqualifies')
+      ],
 
     ];
     //$form['your_message'] = [
@@ -66,10 +76,26 @@ class ModuleConfigurationForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $values = $form_state->getValues();
+    //$values = $form_state->getValues();
     $this->config('gbls_blocks.settings')
-      ->set('variable_name', $values)
+      ->set('poverty_base', $form_state->getValue('poverty_base'))
       ->save();
+    $this->config('gbls_blocks.settings')
+      ->set('poverty_increment', $form_state->getValue('poverty_increment'))
+      ->save();
+    $this->config('gbls_blocks.settings')
+      ->set('checker_title', $form_state->getValue('checker_title'))
+      ->save();
+    $this->config('gbls_blocks.settings')
+      ->set('checker_description', $form_state->getValue('checker_description'))
+      ->save();
+    $this->config('gbls_blocks.settings')
+      ->set('checker_qualifies', $form_state->getValue('checker_qualifies'))
+      ->save();
+    $this->config('gbls_blocks.settings')
+      ->set('checker_disqualifies', $form_state->getValue('checker_disqualifies'))
+      ->save();
+
     parent::submitForm($form, $form_state);
   }
 
